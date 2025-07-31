5087ac522256410f139745a1efb571d014c0a753
poseiso <firdausfajar19@gmail.com>
2025-08-01 06:44:28 +0700

    feat(theme-implement): add theme

---
+++ app/theme/typography.ts
@@ -0,0 +1,52 @@
+
+import { Platform } from "react-native"
+import {
+  SpaceGrotesk_300Light as spaceGroteskLight,
+  SpaceGrotesk_400Regular as spaceGroteskRegular,
+  SpaceGrotesk_500Medium as spaceGroteskMedium,
+  SpaceGrotesk_600SemiBold as spaceGroteskSemiBold,
+  SpaceGrotesk_700Bold as spaceGroteskBold,
+} from "@expo-google-fonts/space-grotesk"
+
+export const customFontsToLoad = {
+  spaceGroteskLight,
+  spaceGroteskRegular,
+  spaceGroteskMedium,
+  spaceGroteskSemiBold,
+  spaceGroteskBold,
+}
+
+const fonts = {
+  spaceGrotesk: {
+    light: "spaceGroteskLight",
+    normal: "spaceGroteskRegular",
+    medium: "spaceGroteskMedium",
+    semiBold: "spaceGroteskSemiBold",
+    bold: "spaceGroteskBold",
+  },
+  helveticaNeue: {
+    thin: "HelveticaNeue-Thin",
+    light: "HelveticaNeue-Light",
+    normal: "Helvetica Neue",
+    medium: "HelveticaNeue-Medium",
+  },
+  courier: {
+    normal: "Courier",
+  },
+  sansSerif: {
+    thin: "sans-serif-thin",
+    light: "sans-serif-light",
+    normal: "sans-serif",
+    medium: "sans-serif-medium",
+  },
+  monospace: {
+    normal: "monospace",
+  },
+}
+
+export const typography = {
+  fonts,
+  primary: fonts.spaceGrotesk,
+  secondary: Platform.select({ ios: fonts.helveticaNeue, android: fonts.sansSerif }),
+  code: Platform.select({ ios: fonts.courier, android: fonts.monospace }),
+}
